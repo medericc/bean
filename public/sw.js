315aa0a6ac43878f2c0acb836e43bb2ce1128b23
medericc <bleublanctour1@>
2025-11-18 04:24:48 +0100

    ee

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return (
+        res ||
+        fetch(event.request).catch(() =>
+          caches.match("/offline.html")
+        )
+      );
+    })
+  );
+});
