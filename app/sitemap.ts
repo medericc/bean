7dfeb48658a240d416c825a6f3317a4eacdd923b
medericc <bleublanctour1@>
2026-08-03 01:17:30 +0200

    ee

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://bearn.vercel.app";
+  const baseUrl = "https://www.histoirebearn.fr/";
 
   return [
     {
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
     },
     {
-      url: `${baseUrl}/histoire`,
+      url: `${baseUrl}/histoire-condense`,
       lastModified: new Date(),
     },
     {
