3424736d165cd70da242bcdd104349fa7fc7a122
medericc <bleublanctour1@>
2026-09-21 01:42:09 +0200

    ee

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.histoirebearn.fr/";
+  const baseUrl = "https://www.histoirebearn.fr";
 
   return [
     {
